3708cad0513a7453e05bb3fff6c0ae0a980afc5a
maxi095 <pereyramaxi095@gmail.com>
2025-05-30 17:48:03 -0300

    vite.config original

--- client/vite.config.js
+++ client/vite.config.js
@@ -8,16 +8,6 @@ export default defineConfig({
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
-
-      // Agregamos esta línea para que Vite encuentre el bundle correcto de html2pdf.js
-      'html2pdf.js$': path.resolve(
-        __dirname,
-        'node_modules/html2pdf.js/dist/html2pdf.bundle.min.js'
-      ),
     },
-  },
-  optimizeDeps: {
-    include: ['html2pdf.js'],
-    include: ['react-icons/fa']
   },
 });
